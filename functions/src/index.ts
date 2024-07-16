0fd90bd2f8da07340b4578b8ef7d5ca817a24663
Yulia Hernandez <iuliiahernandez2109@gmail.com>
2024-07-16 09:58:12 -0500

    added scheduled functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,25 @@ import * as functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
 import accountInfoRouter from "./routes/accountInfoRouter";
+import { getClient } from "./db";
+import AccountInfo from "./models/AccountInfo";
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", accountInfoRouter);
 export const api = functions.https.onRequest(app);
+
+exports.scheduledFunctionCrontab = functions.pubsub
+  .schedule("0 0 * * *")
+  .timeZone("America/New_York")
+  .onRun(async () => {
+    try {
+      const client = await getClient();
+      client
+        .db()
+        .collection<AccountInfo>("myRecipes")
+        .updateMany({}, { $set: { meals: [] } });
+    } catch (error) {
+      console.log(error, "internal server error");
+    }
+  });
